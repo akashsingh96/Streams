e00f4e4b0ba73b9be2371942374e1e758d58cfc8
akashsingh96 <akash96317@gmail.com>
2019-07-25 15:49:12 +0530

    connecting headers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import {BrowserRouter,Route,Link} from 'react-router-dom'
+import {BrowserRouter,Route} from 'react-router-dom'
+
 import StreamList from './streams/StreamList'
 import StreamCreate from './streams/StreamCreate'
 import StreamShow from './streams/StreamShow'
 import StreamEdit from './streams/StreamEdit'
 import StreamDelete from './streams/StreamDelete'
-
+import Header from './Header'
 
 
 
@@ -14,7 +15,8 @@ const App=()=>{
     return(
         <div>
             <BrowserRouter>
-                <div>
+                <div className='ui container'>
+                    <Header/>
                     <Route path='/' exact component={StreamList}/>
                     <Route path='/streams/new' component={StreamCreate}/>
                     <Route path='/streams/edit' component={StreamEdit}/>
