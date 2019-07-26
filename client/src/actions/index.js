bf3c040360e67ed9015bf095cb1f2eeef6de8da6
akashsingh96 <akash96317@gmail.com>
2019-07-26 18:40:58 +0530

    creating streams through action creators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
-import {SIGN_IN,SIGN_OUT} from './types'
+import {SIGN_IN,SIGN_OUT,CREATE_STREAM} from './types'
+import streams from '../apis/streams';
 
 export const trySignIn=(userId)=>{
     return {
@@ -11,4 +12,10 @@ export const trySignOut=(userId)=>{
     return{
         type:SIGN_OUT
     }
+}
+
+export const createStream=formValues=>async dispatch=>{
+        const response=await streams.post('/streams',formValues)
+
+        dispatch({type:CREATE_STREAM,action:response.data})
 }
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import {Field,reduxForm} from 'redux-form'
+import {connect} from 'react-redux'
+
+import {createStream} from '../../actions/index'
 class  StreamCreate extends React.Component{
 
     renderError({error,touched}){
@@ -23,9 +26,11 @@ class  StreamCreate extends React.Component{
         
         )
     }
-    onSubmit(formValues){
-        console.log(formValues)
+
+    onSubmit=formValues=>{
+        this.props.createStream(formValues)
     }
+
     render(){
         return(
             <form className='ui form error' onSubmit={this.props.handleSubmit(this.onSubmit)}>
@@ -46,7 +51,10 @@ const validate=formValues=>{
     return errors
 }
 
-export default reduxForm({
+const formWrapped= reduxForm({
     form:'streamCreate',
     validate
 })(StreamCreate)
+
+
+export default connect(null,{createStream})(formWrapped)
